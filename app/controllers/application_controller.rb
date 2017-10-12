8dab973686d9ed3d01a1187a488863920e32245b
Chris Nyaga <nyagachris75@gmail.com>
2017-10-12 16:30:13 +0300

    Created a method to prevent a user from accessing some routes in the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def signed_in
+    if !user_signed_in?
+      flash[:alert]='Please Sign In First!'
+      redirect_to new_user_session_path(after:'dashboard')
+    end
+  end
 end
