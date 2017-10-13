3ca31b8e18333d6786babe2ca2321d8984b30fa6
Chris Nyaga <nyagachris75@gmail.com>
2017-10-13 10:51:37 +0300

    Changed to a verified madrill app domain.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
-    domain: "zegetech.com",
+    domain: "bigzoo.me",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "Restaurant",
