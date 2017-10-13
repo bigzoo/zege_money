b4e2209adeed0e807d662bf21be830b43d1c1757
Chris Nyaga <nyagachris75@gmail.com>
2017-10-13 08:34:57 +0300

    Updated database schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012134635) do
+ActiveRecord::Schema.define(version: 20171012145219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20171012134635) do
     t.string "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "receiver_identifier"
+    t.string "receiver_identifier_type"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,7 +50,7 @@ ActiveRecord::Schema.define(version: 20171012134635) do
     t.string "last_name"
     t.string "phone_number"
     t.string "phone_number_verified"
-    t.string "balance"
+    t.string "balance", default: "0"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
