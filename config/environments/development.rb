f8cb4192034c0cc0ed04511640f9da8831aac525
Chris Nyaga <nyagachris75@gmail.com>
2017-10-20 10:46:35 +0300

    Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -27,7 +28,27 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.mandrillapp.com",
+  #   port: 587,
+  #   domain: "zegetech.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: "Restaurant",
+  #   password: ENV["SMTP_PASSWORD"]
+  #  }
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "railscasts.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "nyagachris75@gmail.com",
+  password: "ChrisHueyJones"
+}
+
 
   config.action_mailer.perform_caching = false
 
